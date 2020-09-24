a9c1a1fea9f2aefcfeef3f9593a78247820b87dd
zili.peng <2059728+openmartin@users.noreply.github.com>
2020-09-24 12:41:37 +0800

    speed up file read (speedup not obvious)

--- rest/src/main/java/fr/pilato/elasticsearch/crawler/fs/rest/DownloadApi.java
+++ rest/src/main/java/fr/pilato/elasticsearch/crawler/fs/rest/DownloadApi.java
@@ -2,20 +2,14 @@ package fr.pilato.elasticsearch.crawler.fs.rest;
 
 import fr.pilato.elasticsearch.crawler.fs.client.ElasticsearchClient;
 import fr.pilato.elasticsearch.crawler.fs.settings.FsSettings;
-import org.apache.http.client.utils.URIUtils;
-import org.apache.logging.log4j.util.Base64Util;
-import org.glassfish.jersey.media.multipart.FormDataParam;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.File;
-import java.io.UnsupportedEncodingException;
+import java.io.IOException;
 import java.net.URLDecoder;
-import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Base64;
 
 @Path("/fetch")
 public class DownloadApi extends RestApi {
@@ -30,7 +24,7 @@ public class DownloadApi extends RestApi {
 
     @GET
     @Produces(MediaType.APPLICATION_OCTET_STREAM)
-    public Response get(@QueryParam("path") String path) throws UnsupportedEncodingException {
+    public Response get(@QueryParam("path") String path) throws IOException {
         path = URLDecoder.decode(path, "UTF-8");
         String rootFolder = this.settings.getFs().getUrl();
         java.nio.file.Path fullPath = Paths.get(rootFolder, path);
@@ -39,8 +33,8 @@ public class DownloadApi extends RestApi {
         if (!Files.exists(fullPath)) {
             throw new NotFoundException();
         } else {
-            File file = fullPath.toFile();
-            return Response.ok(file).build();
+            byte[] content = Files.readAllBytes(fullPath);
+            return Response.ok(content).build();
         }
     }
 
